8856a136c449c2446974f0f0266d82bdb54068b6
fdfiroz <firozahamed0@gmail.com>
2023-11-25 01:49:56 +0600

    Add JWT authentication and user endpoints

--- index.js
+++ index.js
@@ -49,6 +49,58 @@ async function run() {
   }
   run().catch(console.dir);
 
+
+const database = client.db("mcms");
+const userCollection = database.collection("userCollection");
+const campCollection = database.collection("campCollection");
+
+  
+
+  //JWT Middleware
+app.post("/api/v1/auth/access-token", async (req, res) => {
+  const body = req.body;
+  //   jwt.sign("payload", "secretKey", "expireInfo");
+  // user: abc@gmail.com
+  const token = jwt.sign(body, process.env.ACCESS_TOKEN, { expiresIn: "10h" });
+  const expirationDate = new Date(); // Create a new Date object
+  expirationDate.setDate(expirationDate.getDate() + 365); // Set the expiration date to 365 days from the current date
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      expires: expirationDate,
+    })
+    .send({ massage: "success" });
+});
+//logout
+app.post("/api/v1/auth/logout", async (req, res) => {
+  try{
+    const user = req.body;
+  res.clearCookie("token", { maxAge: 0 }).send({ message: "success" });
+  }
+  catch{
+    console.log(error)
+  }
+});
+
+app.post("/api/v1/users", async (req, res) =>{
+  const user = req.body;
+  console.log(user)
+  const result = await userCollection.insertOne(user);
+  res.send(result);
+})
+
+app.get("/api/v1/users/role/:email", async (req, res) => {
+try{
+  const email = req.params.email
+  const result = await userCollection.findOne({email})
+  res.send(result.role)
+}
+catch(error){
+  console.log(error)
+}
+});
 //Start server
   app.get('/', (req, res) => {
     res.send('Hello from MCMS Server.')
